b72f8704fb1dde74736f4872fa8e6fe862f9960f
Emmet Spaeth <emmet.spaeth@gmail.com>
2024-03-20 14:32:08 -0400

    creating fields and getter for Player class

--- Player.java
+++ Player.java
@@ -1,3 +1,19 @@
 public class Player {
 
+  private double hp, score;
+  private String[] inventory;
+
+  public Player() {}
+
+  public double getHp() {
+    return hp;
+  }
+
+  public double getScore() {
+    return score;
+  }
+
+  public String[] getInventory() {
+    return inventory;
+  }
 }
