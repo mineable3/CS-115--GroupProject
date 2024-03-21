1bf639a785daa7a700640df8ddfc4aadd2da3e2e
LyaPolitan <lyamperez04@gmail.com>
2024-03-21 09:39:38 -0400

    changed to ints

--- Enemy.java
+++ Enemy.java
@@ -3,12 +3,16 @@
  * We will use this class when making all of the different enemies
  */
 
- public class Enemy {
+ public abstract class Enemy {
 
   protected boolean alive;
-  protected double hp, damage, pointReward;
+  protected int hp, damage, pointReward;
 
-  public Enemy() {}
+  public Enemy(int hp, int damage, int pointReward) {
+    this.hp = hp;
+    this.damage = damage;
+    this.pointReward = pointReward;
+  }
 
   public boolean isAlive() {
     return alive;
