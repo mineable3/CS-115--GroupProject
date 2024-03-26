f7cf208692f39f2a9b3c432bea2d128e0bfaf83a
Emmet Spaeth <emmet.spaeth@gmail.com>
2024-03-26 09:45:08 -0400

    starting level 1 + player utility

--- Player.java
+++ Player.java
@@ -1,19 +1,72 @@
 public class Player {
 
-  private double hp, score;
+  private int hp, score;
   private String[] inventory;
 
-  public Player() {}
+  public Player(int hp) {
+    this.hp = hp;
+    inventory = new String[0];
+  }
 
-  public double getHp() {
+  public int getHp() {
     return hp;
   }
 
-  public double getScore() {
+  public void setHp(int hp) {
+    this.hp = hp;
+  }
+
+  public int getScore() {
     return score;
+  }
+
+  public void setScore(int score) {
+    this.score = score;
   }
 
   public String[] getInventory() {
     return inventory;
+  }
+
+  public void displayInventory() {
+    for(String item : inventory) {
+      System.out.print(item + "\t");
+    }
+    System.out.println();
+  }
+
+  public boolean hasItem(String target) {
+    for(String item : inventory) {
+      if(target.equals(item)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public void addItem(String loot) {
+    String[] tempInventory = new String[inventory.length+1];
+
+    for(int i = 0; i < inventory.length; i++) {
+      tempInventory[i] = inventory[i];
+    }
+
+    tempInventory[tempInventory.length - 1] = loot;
+    inventory = tempInventory;
+  }
+
+  public void removeItem(String target) {
+    String[] tempInventory = new String[inventory.length-1];
+
+    for(int i = 0; i < inventory.length; i++) {
+      if(!target.equals(inventory[i])) {
+        tempInventory[i] = inventory[i];
+      }
+    }
+    inventory = tempInventory;
+  }
+
+  public String toString() {
+    return "HP: " + hp + " Points: " + score;
   }
 }
