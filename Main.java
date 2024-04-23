4bed3490f18dae41ab5f887b47de66281818b666
Emmet Spaeth <emmet.spaeth@gmail.com>
2024-04-23 09:24:14 -0400

    start of game info + banner

--- Main.java
+++ Main.java
@@ -12,6 +12,21 @@ public class Main {
     Dungeon1 level1 = new Dungeon1();
     Dungeon2 level2 = new Dungeon2();
     Dungeon3 level3 = new Dungeon3();
+    System.out.println("    _           _                _       _   _              __   _                               _");
+    System.out.println("   | |         | |              (_)     | | | |            / _| | |                             | |");
+    System.out.println("   | |     __ _| |__  _   _ _ __ _ _ __ | |_| |__     ___ | |_  | |     ___  __ _  ___ _ __   __| |___");
+    System.out.println("   | |    / _` | '_ \\| | | | '__| | '_ \\| __| '_ \\   / _ \\|  _| | |    / _ \\/ _` |/ _ \\ '_ \\ / _` / __|");
+    System.out.println("   | |___| (_| | |_) | |_| | |  | | | | | |_| | | | | (_) | |   | |___|  __/ (_| |  __/ | | | (_| \\__ \\");
+    System.out.println("   \\_____/\\__,_|_.__/ \\__, |_|  |_|_| |_|\\__|_| |_|  \\___/|_|   \\_____/\\___|\\__, |\\___|_| |_|\\__,_|___/");
+    System.out.println("                       __/ |                                                 __/ |");
+    System.out.println("                      |___/                                                 |___/");
+
+    System.out.println("info:");
+    System.out.println("Run: allows you to flee from battle");
+    System.out.println("use_item: allows you to use items you have found");
+    System.out.println("show_item: allows you to check the items you have found");
+    System.out.println("look: searches the room for anything of use (does not pick up the item, can be used multiple times)");
+    System.out.println("pickup: picks up items you have found in the room (must use look before picking an item up, can be used multiple times)");
     tutorial.run(p, keyboard);
     // p.addItem("short_sword");
     // p.addItem("small_shield (passive)");
